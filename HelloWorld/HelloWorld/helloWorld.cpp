5ced5f84695736b3d745573d543b55066360a7cd
Byung Wook <quddnr153@gmail.com>
2015-12-11 00:59:10 +0900

    My first commit of hello project.

---
+++ HelloWorld/HelloWorld/helloWorld.cpp
@@ -0,0 +1,27 @@
+/*
+	2015.12.11 (Fri.)
+
+	Autor : Byungwook Lee
+
+	File name : hellowWorld.cpp
+
+	Description : This is my first project of visual studio with git and github.
+				  It just print out "Hello World!".
+
+	Compile environment : Window 7 64bit, visual studio 2015
+
+	Bild command in Visual studio : Ctrl + Shift + B
+	Execution command in Visual studio : Ctrl + F5
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	char * hello = "Hello World\n";
+	cout << hello;
+
+	return 0;
+}
